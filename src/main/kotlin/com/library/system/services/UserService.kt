62861b2a699ec12e1916e767064cd101dd929d5a
ram <maheshwariiram@gmail.com>
2025-04-22 03:59:33 +0200

    feat: services

--- src/main/kotlin/com/library/system/services/UserService.kt
+++ src/main/kotlin/com/library/system/services/UserService.kt
@@ -1,13 +1,29 @@
 package com.library.system.services
 
+import com.library.system.model.ResourceNotFoundException
 import com.library.system.model.User
+import com.library.system.repository.UserRepository
+import org.springframework.stereotype.Service
+import java.util.UUID
 
-class UserService {
-    fun getUserById(i: Int) {
+@Service
+class UserService(private val userRepository: UserRepository) {
 
+    fun registerUser(user: User): User {
+        return userRepository.save(user)
     }
 
-    fun updateUser(user: User){
+    fun getUserById(id: UUID): User {
+        return userRepository.findById(id)
+            .orElseThrow { ResourceNotFoundException("User with ID $id not found") }
+    }
 
+    fun updateUser(id: UUID, userDetails: User): User {
+        val existingUser = getUserById(id)
+
+        // Create a new user object with updated fields but keep the original ID
+        val userToUpdate = userDetails.copy(id = existingUser.id)
+
+        return userRepository.save(userToUpdate)
     }
 }
